7108e92e0ad939dd8d0dfc7f7dcd8c330146a2c2
Noah Gallet <la215736@student.helha.be>
2025-05-13 20:14:02 +0200

    ajout d'unne méthode dans LanternaUtils pour afficher une popup avec un input et qui renvoit le contenu de cet input

--- client/src/main/java/be/helha/poo3/utils/LanternaUtils.java
+++ client/src/main/java/be/helha/poo3/utils/LanternaUtils.java
@@ -9,6 +9,7 @@ import com.googlecode.lanterna.screen.Screen;
 
 import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class LanternaUtils {
 
@@ -68,6 +69,39 @@ public class LanternaUtils {
         mainPanel.addComponent(buttonsPanel);
         menuWindow.setComponent(mainPanel);
         gui.addWindowAndWait(menuWindow);
+    }
+
+    public String openPopupWithTextField(String title, String message) {
+        AtomicReference<String> result = new AtomicReference<>(null);
+        BasicWindow menuWindow = new BasicWindow(title);
+        menuWindow.setHints(List.of(Window.Hint.CENTERED));
+        Panel mainPanel = new Panel(new LinearLayout(Direction.VERTICAL));
+        mainPanel.setPreferredSize(new TerminalSize(20, 5));
+        mainPanel.addComponent(new Label(message));
+
+        TextBox input = new TextBox()
+                .setPreferredSize(new TerminalSize(20, 1));
+
+        mainPanel.addComponent(input);
+        mainPanel.addComponent(new EmptySpace());
+
+        Panel buttonsPanel = new Panel(new LinearLayout(Direction.HORIZONTAL));
+        buttonsPanel.addComponent(new Button("Ok", () -> {
+            result.set(input.getText());
+            menuWindow.close();
+        }));
+
+        buttonsPanel.addComponent(new EmptySpace(new TerminalSize(2, 1)));
+
+        buttonsPanel.addComponent(new Button("Non", () -> {
+            result.set(null);
+            menuWindow.close();
+        }));
+        mainPanel.addComponent(buttonsPanel);
+
+        menuWindow.setComponent(mainPanel);
+        gui.addWindowAndWait(menuWindow);
+        return result.get();
 
     }
 }
