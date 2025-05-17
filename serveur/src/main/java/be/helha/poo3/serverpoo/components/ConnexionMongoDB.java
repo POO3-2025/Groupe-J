7950cd2221438936fdad014b4da147a887dc244e
hus0w <hsynozudogru@outlook.com>
2025-05-17 18:44:24 +0200

    Ajout de la configuration JSON pour MongoDB et MySQL + injection manuelle via ConfigurationDB.

--- serveur/src/main/java/be/helha/poo3/serverpoo/components/ConnexionMongoDB.java
+++ serveur/src/main/java/be/helha/poo3/serverpoo/components/ConnexionMongoDB.java
@@ -1,34 +1,60 @@
+// Fichier : ConnexionMongoDB.java
 package be.helha.poo3.serverpoo.components;
 
-import be.helha.poo3.serverpoo.configuration.MongoConfig;
+import be.helha.poo3.serverpoo.configuration.ConfigurationDB;
+import be.helha.poo3.serverpoo.configuration.ConfigurationDB.Databases.Details;
 import com.mongodb.client.*;
 import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
 import org.bson.Document;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 @Component
 public class ConnexionMongoDB {
-    private final MongoConfig config;
-    protected MongoCollection<Document> collection;
-    private MongoClient mongoClient;
 
-    public ConnexionMongoDB(MongoConfig config) {
-        this.config = config;
+    private final ConfigurationDB configurationDB;
+    private MongoClient mongoClient;
+    protected MongoCollection<Document> collection;
+
+    // "production" ou "test" selon ton contexte
+    private static final String ENV_KEY = "production";
+
+    public ConnexionMongoDB(ConfigurationDB configurationDB) {
+        this.configurationDB = configurationDB;
     }
 
     @PostConstruct
     public void init() {
-        String fullUri = String.format("mongodb://%s:%s@localhost:27017", config.getUsername(), config.getPassword());
-        mongoClient = MongoClients.create(fullUri);
-        MongoDatabase database = mongoClient.getDatabase(config.getDb());
-        this.collection = database.getCollection(config.getCollection());
-        System.out.println("Connected to " + config.getDb() + "/" + config.getCollection());
+        try {
+            Details config = configurationDB.getDatabases().getMongoDB().get(ENV_KEY);
+
+            String uri = String.format(
+                    "mongodb://%s:%s@%s:%d",
+                    config.getUser(),
+                    config.getPassword(),
+                    config.getHost(),
+                    config.getPort()
+            );
+
+            mongoClient = MongoClients.create(uri);
+            MongoDatabase database = mongoClient.getDatabase(config.getDatabase());
+            this.collection = database.getCollection("exampleCollection"); // change le nom si besoin
+
+            System.out.println("Connecté à MongoDB: " + config.getDatabase());
+        } catch (Exception e) {
+            System.err.println("Erreur de la connexion à MongoDB: " + e.getMessage());
+        }
     }
 
     public MongoCollection<Document> getCollection() {
         return collection;
     }
-}
 
+    @PreDestroy
+    public void close() {
+        if (mongoClient != null) {
+            mongoClient.close();
+            System.out.println("MongoClient closed.");
+        }
+    }
+}
